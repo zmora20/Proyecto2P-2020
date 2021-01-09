1a76f2ba5636d9ff713d697f4051133177ac018d
Zaida Mora <zmora@espol.edu.ec>
2021-01-09 17:57:18 -0500

    Creacion clases y primera interfaz

---
+++ src/main/java/Restaurante/Usuario/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Restaurante.Usuario;
+
+/**
+ *
+ * @author zaida
+ */
+public class Usuario {
+    private String correoElectronico;
+    private String contrasenia;
+    
+    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pooespol.proy2p {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.pooespol.proy2p to javafx.fxml;
     exports com.pooespol.proy2p;
