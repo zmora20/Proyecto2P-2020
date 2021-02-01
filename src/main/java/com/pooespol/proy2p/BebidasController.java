5a7d5948249cece0c983d3b472ab913bb5becebb
Zaida Mora <zmora@espol.edu.ec>
2021-01-31 23:02:09 -0500

    Actualizacion menu

--- src/main/java/com/pooespol/proy2p/BebidasController.java
+++ src/main/java/com/pooespol/proy2p/BebidasController.java
@@ -10,92 +10,63 @@ import Restaurante.restaurante;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
 
 /**
  * FXML Controller class
  *
- * @author Felipe
+ * @author zaida
  */
 public class BebidasController implements Initializable {
-    
+
     @FXML
-    private TextField nombreBebida;
-    
-    @FXML
-    private FlowPane x;
+    private FlowPane panelbebidas;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-    }
-
-    /*
-    @FXML
-    private void mostrarBebidas (ActionEvent event) {        
-//obtener el elemento que fue seleccionado en el combo
-        
-        
-        //elemino el contenido anterios del flowPane
-        
-        panelBebidas.getChildren().clear();
-        
-        /*
-        //mostrar en el FlowPane panelPeliculas el nombre del genero seleccionado
-        Label l = new Label(genero.getNombre());
-        panelPeliculas.getChildren().add(l);
-        
->>>>>>> Stashed changes
+        // TODO
         try {
-            //obtengo la lista de peliculas del genero seleccionado
-            List<Platos> bebidas = restaurante.menu;
-            for(Platos p: bebidas){
-                VBox vboxbebida = new VBox();
+              ArrayList<Platos> bebida= restaurante.leerbebidas();
+            for(Platos p: bebida){
+                HBox hboxbebida = new HBox();
                 //crear la imagen
                 InputStream inputImg= App.class.getResource(p.getImagen()).openStream();
                 ImageView imgv = new ImageView(new Image(inputImg));
-                vboxbebida.getChildren().add(imgv);
+                hboxbebida.getChildren().add(imgv);
                 
-                //crea el label del nombre y lo agrego al VBox
                 Label lnombre = new Label(p.getNombrePlato());
-                vboxbebida.getChildren().add(lnombre);
-                String pre=String.valueOf(p.getPrecio());
-                Label lprecio = new Label(pre);
-                vboxbebida.getChildren().add(lprecio);
-
-                              
-                //vboxpelicula.setPadding(new Insets(2,3,3,4));
+                hboxbebida.getChildren().add(lnombre);
+                Label lprecio = new Label(String.valueOf(p.getPrecio()));
+                hboxbebida.getChildren().add(lprecio);
                 
-                //agregamos el VBox de la pelicula al FlowPane
-                x.getChildren().add(vboxbebida);
+                hboxbebida.setPadding(new Insets(21,0,21,0));
+                
+                
+                panelbebidas.getChildren().add(hboxbebida);
                 
             }
         } catch (IOException ex) {
             ex.printStackTrace();
-        }
+        } 
         
         
         
         
-        
-        
-    } */
-    
-    
+    }    
     
 }
